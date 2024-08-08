7ab0ae63add990b494bac285a58cf9700a1e0cde
jichun88 <kwoodong@gmail.com>
2024-08-08 12:19:09 +0900

    Initial commit

---
+++ test/cdk-ecs-ecr-pipeline.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkEcsEcrPipeline from '../lib/cdk-ecs-ecr-pipeline-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-ecs-ecr-pipeline-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkEcsEcrPipeline.CdkEcsEcrPipelineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
